916c8ee4bf2b3443a11ea9ec1350013c7ddb129f
Jason Bullers <jason.bullers@canada.ca>
2022-02-02 15:41:51 -0500

    Pass schema and args through instead of using fields. Some formatting changes, inline methods, and remove `usage` since if Args is used wrong, it throws an ArgsException anyway.

--- src/main/java/refactor/Args.java
+++ src/main/java/refactor/Args.java
@@ -9,114 +9,96 @@ import static java.lang.Integer.parseInt;
 
 public class Args {
 
-    private final String schema;
-    private final String[] args;
     private final Map<Character, Boolean> booleanArgs = new HashMap<>();
     private final Map<Character, String> stringArgs = new HashMap<>();
     private final Map<Character, Integer> intArgs = new HashMap<>();
+
     private final Set<Character> argsFound = new HashSet<>();
+
     private int currentArgument;
 
     public Args(String schema, String[] args) throws ArgsException {
-        this.schema = schema;
-        this.args = args;
-        parse();
+        parseSchema(schema);
+        parseArguments(args);
     }
 
-    private void parse() throws ArgsException {
-        if (schema.isEmpty() && args.length == 0) return;
-        parseSchema();
-        parseArguments();
-    }
-
-    private void parseSchema() throws ArgsException {
+    private void parseSchema(String schema) throws ArgsException {
         for (String element : schema.split(",")) {
             if (!element.isEmpty()) {
                 String trimmedElement = element.trim();
-                parseSchemaElement(trimmedElement);
+                char elementId = trimmedElement.charAt(0);
+                String elementTail = trimmedElement.substring(1);
+                parseSchemaElement(validateSchemaElementId(elementId, schema), elementTail);
             }
         }
     }
 
-    private void parseSchemaElement(String element) throws ArgsException {
-        char elementId = element.charAt(0);
-        String elementTail = element.substring(1);
-        validateSchemaElementId(elementId);
-        if (isBooleanSchemaElement(elementTail))
-            parseBooleanSchemaElement(elementId);
-        else if (isStringSchemaElement(elementTail))
-            parseStringSchemaElement(elementId);
-        else if (isIntegerSchemaElement(elementTail)) {
-            parseIntegerSchemaElement(elementId);
+    private static char validateSchemaElementId(char elementId, String schema) throws ArgsException {
+        if (Character.isLetter(elementId)) {
+            return elementId;
         } else {
-            throw new ArgsException("Argument: %c has invalid format: %s.".formatted(elementId, elementTail));
-        }
-    }
-
-    private void validateSchemaElementId(char elementId) throws ArgsException {
-        if (!Character.isLetter(elementId)) {
             throw new ArgsException("Bad character: %c in Args format: %s".formatted(elementId, schema));
         }
     }
 
-    private void parseBooleanSchemaElement(char elementId) {
-        booleanArgs.put(elementId, false);
-    }
-
-    private void parseIntegerSchemaElement(char elementId) {
-        intArgs.put(elementId, 0);
-    }
-
-    private void parseStringSchemaElement(char elementId) {
-        stringArgs.put(elementId, "");
-    }
-
-    private boolean isStringSchemaElement(String elementTail) {
-        return elementTail.equals("*");
+    private void parseSchemaElement(char elementId, String elementTail) throws ArgsException {
+        if (isBooleanSchemaElement(elementTail))
+            booleanArgs.put(elementId, false);
+        else if (isStringSchemaElement(elementTail))
+            stringArgs.put(elementId, "");
+        else if (isIntegerSchemaElement(elementTail)) {
+            intArgs.put(elementId, 0);
+        } else {
+            throw new ArgsException("Argument: %c has invalid format: %s.".formatted(elementId, elementTail));
+        }
     }
 
     private boolean isBooleanSchemaElement(String elementTail) {
         return elementTail.isEmpty();
     }
 
+    private boolean isStringSchemaElement(String elementTail) {
+        return elementTail.equals("*");
+    }
+
     private boolean isIntegerSchemaElement(String elementTail) {
         return elementTail.equals("#");
     }
 
-    private void parseArguments() throws ArgsException {
+    private void parseArguments(String[] args) throws ArgsException {
         for (currentArgument = 0; currentArgument < args.length; currentArgument++) {
             String arg = args[currentArgument];
-            parseArgument(arg);
+            parseArgument(arg, args);
         }
     }
 
-    private void parseArgument(String arg) throws ArgsException {
+    private void parseArgument(String arg, String[] args) throws ArgsException {
         if (arg.startsWith("-")) {
-            parseElements(arg);
+            parseElements(arg, args);
         }
     }
 
-    private void parseElements(String arg) throws ArgsException {
+    private void parseElements(String arg, String[] args) throws ArgsException {
         for (int i = 1; i < arg.length(); i++) {
-            parseElement(arg.charAt(i));
+            parseElement(arg.charAt(i), args);
         }
     }
 
-    private void parseElement(char argChar) throws ArgsException {
-        if (setArgument(argChar)) {
+    private void parseElement(char argChar, String[] args) throws ArgsException {
+        if (setArgument(argChar, args)) {
             argsFound.add(argChar);
         } else {
             throw new ArgsException("Argument -%c unexpected".formatted(argChar));
         }
     }
 
-    private boolean setArgument(char argChar) throws ArgsException {
+    private boolean setArgument(char argChar, String[] args) throws ArgsException {
         if (isBooleanArg(argChar))
             setBooleanArg(argChar, true);
         else if (isStringArg(argChar))
-            setStringArg(argChar);
+            setStringArg(argChar, args);
         else if (isIntArg(argChar))
-            setIntArg(argChar);
+            setIntArg(argChar, args);
         else
             return false;
 
@@ -125,7 +107,7 @@ public class Args {
 
     private boolean isIntArg(char argChar) {return intArgs.containsKey(argChar);}
 
-    private void setIntArg(char argChar) throws ArgsException {
+    private void setIntArg(char argChar, String[] args) throws ArgsException {
         currentArgument++;
         String parameter = null;
         try {
@@ -138,7 +120,7 @@ public class Args {
         }
     }
 
-    private void setStringArg(char argChar) throws ArgsException {
+    private void setStringArg(char argChar, String[] args) throws ArgsException {
         currentArgument++;
         try {
             stringArgs.put(argChar, args[currentArgument]);
@@ -161,10 +143,6 @@ public class Args {
 
     public int cardinality() {
         return argsFound.size();
-    }
-
-    public String usage() {
-        return !schema.isEmpty() ? "-[" + schema + "]" : "";
     }
 
     public String getString(char arg) {
