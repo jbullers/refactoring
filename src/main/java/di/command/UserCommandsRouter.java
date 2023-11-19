73485baba49ec9cfd72e28897acee891fde4950e
Jason Bullers <jason.bullers@gmail.com>
2023-11-19 15:12:21 -0500

    Initial commit of the Dagger tutorial code. I organized the classes in to packages (everything was in the same package originally) because I thought it might make things a little more realistic as a starting point and remove on potential point of complexity with understanding the pieces of the application.

---
+++ src/main/java/di/command/UserCommandsRouter.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2019 The Dagger Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package di.command;
+
+import dagger.BindsInstance;
+import dagger.Module;
+import dagger.Subcomponent;
+import di.PerSession;
+import di.Username;
+import di.amounts.AmountsModule;
+import di.db.AccountModule;
+
+@PerSession
+@Subcomponent(modules = { AccountModule.class, AmountsModule.class, UserCommandsModule.class})
+interface UserCommandsRouter {
+  CommandRouter router();
+
+  @Subcomponent.Factory
+  interface Factory {
+    UserCommandsRouter create(@BindsInstance @Username String username);
+  }
+
+  @Module(subcomponents = UserCommandsRouter.class)
+  interface InstallationModule {}
+}
