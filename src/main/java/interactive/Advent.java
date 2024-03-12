d70ed69deee20a6d149a22a92c6e0c28367459f1
Jason Bullers <jason.bullers@gmail.com>
2024-03-11 20:16:31 -0400

    Bootstrapping interactive programming demo

---
+++ src/main/java/interactive/Advent.java
@@ -0,0 +1,10 @@
+package interactive;
+
+/**
+ * https://adventofcode.com/2021/day/2
+ */
+public class Advent {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/interactive/Server.java
@@ -0,0 +1,12 @@
+package interactive;
+
+import io.javalin.Javalin;
+
+public class Server {
+
+    public static void main(String[] args) {
+        var app = Javalin.create(/*config*/)
+                         .get("/", ctx -> ctx.result("Hello World"))
+                         .start(7070);
+    }
+}
